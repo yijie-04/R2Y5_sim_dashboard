49fdf57c5250eae826b5117ab8951b68f0bec560
yijiewang0806 <yijiewang0806@gmail.com>
2026-01-05 21:07:33 -0500

    add geojson file so don't need to parse every time

--- cicd-dashboard/src/components/OSMMap.jsx
+++ cicd-dashboard/src/components/OSMMap.jsx
@@ -1,13 +1,18 @@
-import React, { useMemo, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { MapContainer, TileLayer, GeoJSON, useMap } from 'react-leaflet'; 
-import osmtogeojson from 'osmtogeojson'; 
 import 'leaflet/dist/leaflet.css';
-import mapData from '../map/mcity.json'; 
-
 import L from 'leaflet';
+
+import geoJsonData from '../map/mcity.geojson'; 
+
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-let DefaultIcon = L.icon({ iconUrl: icon, shadowUrl: iconShadow, iconSize: [25, 41], iconAnchor: [12, 41] });
+let DefaultIcon = L.icon({ 
+  iconUrl: icon, 
+  shadowUrl: iconShadow, 
+  iconSize: [25, 41], 
+  iconAnchor: [12, 41] 
+});
 L.Marker.prototype.options.icon = DefaultIcon;
 
 function MapController({ center, zoom }) {
@@ -19,32 +24,10 @@ function MapController({ center, zoom }) {
 }
 
 export default function OSMMap({ lat, lng, zoom }) {
-  
-  const geoJsonData = useMemo(() => {
-    if (!mapData) return null;
-
-    let converted = mapData;
-    if (mapData.elements) {
-        converted = osmtogeojson(mapData);
-    } else if (mapData.data && mapData.data.elements) {
-        converted = osmtogeojson(mapData.data);
-    }
-
-    if (converted && converted.features) {
-        const cleanFeatures = converted.features.filter(feature => {
-            return feature.geometry && feature.geometry.coordinates;
-        });
-        
-        return {
-            ...converted,
-            features: cleanFeatures
-        };
-    }
-
-    return converted;
-  }, []);
-
-  const isValid = geoJsonData && geoJsonData.type === "FeatureCollection" && geoJsonData.features.length > 0;
+  const isValid = geoJsonData && 
+                  geoJsonData.type === "FeatureCollection" && 
+                  geoJsonData.features && 
+                  geoJsonData.features.length > 0;
 
   return (
     <div className="w-full h-full z-0 bg-[#222]">
@@ -63,12 +46,12 @@ export default function OSMMap({ lat, lng, zoom }) {
             url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
         />
 
-        {isValid ? (
+        {isValid && (
           <GeoJSON 
-             key="clean-map-layer" 
+             key="static-map-layer" 
              data={geoJsonData} 
           />
-        ) : null}
+        )}
       </MapContainer>
     </div>
   );
